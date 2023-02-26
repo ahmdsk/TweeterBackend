c1f4a46268f74b75d49d93fc324c957c87bec9d0
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-02-26 12:53:55 +0700

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express"
 import bodyParser from "body-parser"
 import router from "./routes/router"
 import mongoose from "mongoose"
+import cors from "cors"
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json())
 
 // Routing
 app.use(router)
+app.use(cors())
 
 mongoose.set('strictQuery', true)
 mongoose.connect("mongodb://localhost:27017/tweeter", () => {
